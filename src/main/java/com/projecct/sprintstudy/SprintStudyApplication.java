908cc2bfec643dc244fc618b3d302f9440250caf
ly <3119189483@qq.com>
2023-03-29 23:42:32 +0800

    init

---
+++ src/main/java/com/projecct/sprintstudy/SprintStudyApplication.java
@@ -0,0 +1,13 @@
+package com.projecct.sprintstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projecct/sprintstudy/SprintStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecct.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
